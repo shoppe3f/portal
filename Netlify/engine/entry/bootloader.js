8226edd9c81ddc926ae7286f22409a9d5b74c44b
shoppe3f <sofisyaqilah@gmail.com>
2026-01-08 13:44:28 +0700

    Delete Netlify directory

--- Netlify/engine/entry/bootloader.js
+++
@@ -1,7 +0,0 @@
-// /engine/entry/bootloader.js
-// Loader & popup logic sudah deprecated.
-// File ini dipertahankan untuk konsistensi arsitektur engine.
-
-export default function bootloader() {
-  // no-op (intentionally empty)
-}
